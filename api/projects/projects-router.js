1eeb6cd6c5c8851b2df961e28fd70aa4f0bcf5b5
MaxT6 <maximiliant6@gmail.com>
2022-06-10 18:44:50 -0400

    added response for project id and delete project by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const Projects = require('./projects-model')
 const router = express.Router()
+const { validateUserId } = require('./projects-middleware')
 
 
 // router.get('/api/projects', (req, res, next) => {
@@ -18,14 +19,27 @@ router.get('/', (req, res, next) => {
     res.json(projects)
   })
   .catch(next)
- 
-})
+});
+
+router.get('/:id', validateUserId, (req, res, next) => {
+  res.json(req.project)
+});
+
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    const result = await Projects.remove(req.params.id)
+    res.json(req.user)
+  } catch(err) {
+    next(err)
+  }
+});
+
 
 router.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     customMessage: "There was an error",
     message: err.message
   })
-})
+});
 
 module.exports = router
